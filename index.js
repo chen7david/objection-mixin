f7703864a94b004a50296e4ecf87f3bc33d14720
David <chen7david@me.com>
2020-03-16 17:09:54 +0800

    update

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ module.exports = (Model) => {
         }
 
         static async getById(id){
+            id = id ? parseInt(id, 10) : null
+            if(!id) return null
             return await this.getByKey('id', id)
         }
 
@@ -19,23 +21,32 @@ module.exports = (Model) => {
                 .first()
         }
 
-        async sync(relation, target, customId){
-            const related = await this.$relatedQuery(relation)
-            const original = related.map(el => el.id)
-            const add = notIn(target, original)
-            const remove = notIn(original, target)
-            if(add.length + remove.length == 0) return false
-            const relId = customId ? customId : 
-                pluralize.singular(relation) + '_id'
-            
-            const added = await this
-                .$relatedQuery(relation)
-                .relate(add)
-            const removed = await this
-                .$relatedQuery(relation)
-                .unrelate()
-                .whereIn(relId, remove)
-            return added + removed > 0
+        async $sync(relation, target, customId){
+            try {
+                const result = await BaseModel.transaction(async (trx) => {
+                    const related = await this.$relatedQuery(relation)
+                    const original = related.map(el => el.id)
+                    const add = notIn(target, original)
+                    const remove = notIn(original, target)
+                    if(add.length + remove.length == 0) return false
+                    const relId = customId ? customId : 
+                        pluralize.singular(relation) + '_id'
+                    
+                    const added = await this
+                        .$relatedQuery(relation)
+                        .relate(add)
+                    const removed = await this
+                        .$relatedQuery(relation)
+                        .unrelate()
+                        .whereIn(relId, remove)
+                    return added + removed > 0
+                })
+                return result
+                
+            } catch (err) {
+                console.log(err)
+                return false
+            }
         }
 
         async $beforeInsert(){
