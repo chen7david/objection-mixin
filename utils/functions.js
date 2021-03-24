e60040dec5cadc2c67363f7740a5acee70c10ca1
David Chen <chen7david@me.com>
2021-03-24 10:16:27 +0800

    update

--- utils/functions.js
+++ utils/functions.js
@@ -5,7 +5,7 @@ const self = {
     timestamp: () => new Date().toISOString(),
     notIn: (setA, setB) => setA.filter(el => !setB.includes(el)),
     md5: () => crypto.createHash('md5').update(`${Math.random()}`).digest('hex'),
-    toMiliSeconds: (str) => {
+    toms: (str) => {
 
         const unit = str.slice(str.length - 1)
     
@@ -21,6 +21,42 @@ const self = {
         const magnitude = parseInt(str, 10)
         if(!magnitude) return false
         return magnitude * units[unit] * 1000
+    },
+
+    notIn: (setA, setB) => setA.filter(el => !setB.includes(el)),
+
+    randInt: (min,max) => Math.floor(Math.random() * (max - min + 1)) + min,
+
+    randEl: (array) => {
+        const randInt = self.randInt(0, array.length -1)
+        return array[randInt]
+    },
+
+    randChar: () => {
+        const chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ"
+        return self.randEl(chars)
+    },
+
+    serialInt: (mask) => {
+        let serial = ""
+        if(mask) {
+            for(let i = 0; i < mask.length; i++){
+                let maskChar = mask[i] 
+                serial += maskChar == "0" ? self.randInt(0,9) : maskChar
+            }
+        }
+        return serial
+    },
+
+    serialChar: (mask) => {
+        let serial = ""
+        if(mask) {
+            for(let i = 0; i < mask.length; i++){
+                let maskChar = mask[i] 
+                serial += maskChar == "0" ? self.randChar() : maskChar
+            }
+        }
+        return serial
     },
 }
 
